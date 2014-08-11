6188f0494b588afcd8c1fe5c25ba870377f076cc
Matthew Wagerfield <matthew@wagerfield.com>
2014-08-11 20:03:20 +0100

    :gb: Created colorgasm.js

---
+++ assets/scripts/colorgasm.js
@@ -0,0 +1,24 @@
+(function(window) {
+
+  var COLOURS = [
+    // Base
+    0x1E1A31,
+    // Core
+    0xFFFFFF,
+    // West
+    0x6B2A64,
+    0xB14B77,
+    0xE2687C,
+    0xF98A75,
+    0xFFB46B,
+    0xFFE193,
+    // East
+    0x4C4690,
+    0x4D64B0,
+    0x518CBE,
+    0x5BAFCD,
+    0x72D4E2,
+    0xB9F7F5
+  ];
+
+})(window);
