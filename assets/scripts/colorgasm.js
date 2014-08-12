3234c2ec6efb76fa4ad09a6d4a2f5497f2294be0
Matthew Wagerfield <matthew@wagerfield.com>
2014-08-12 21:05:44 +0100

    :gb: Implemented random wave form for testing

--- assets/scripts/colorgasm.js
+++ assets/scripts/colorgasm.js
@@ -1,6 +1,9 @@
 (function(window) {
 
-  var COLORS = {};
+  var WEST = 'west',
+      EAST = 'east',
+      CORE = 'core',
+      COLORS = {};
 
   function addColorPalette(id, base, core, west, east) {
     COLORS[id] = {base:base, core:core, west:west, east:east};
@@ -27,22 +30,74 @@
 
   Sketch.create({
 
+    interval: 1,
+    container: document.getElementById('container'),
+
     setup: function() {
-      this.container = document.getElementById('container');
-      this.container.style.backgroundColor = COLORS.main.base;
+      this.setColorPalette(COLORS.main);
       this.reset();
     },
 
+    setColorPalette: function(palette) {
+      if (this.palette !== palette) {
+        this.palette = palette;
+        console.log(this);
+        this.container.style.backgroundColor = palette.base;
+      }
+    },
+
     reset: function() {
-      this.wave = [];
+      this.wave = [Math.random()];
+    },
+
+    resize: function() {
+      this.center = Math.round(this.width * 0.5);
     },
 
     update: function() {
+      var sign = Math.random() > 0.5 ? 1 : -1;
+      var value = this.wave[this.wave.length-1];
+      value += Math.random() * sign * 0.05;
+      value = Math.max(value, 0);
+      value = Math.min(value, 1);
+      this.wave.push(value);
+      if (this.wave.length > 100) {
+        this.wave.shift();
+      }
     },
 
     draw: function() {
-    }
+      var i, l;
+      for (i = 0, l = this.palette.west.length; i < l; i++) {
+      }
+      for (i = 0, l = this.palette.east.length; i < l; i++) {
+      }
+      this.drawWave(WEST, 0.8, 20, this.palette.core);
+    },
 
+    drawWave: function(state, scale, step, color) {
+      var c1x, c1y, c2x, c2y;
+      var i = this.wave.length - 1;
+      var x = Math.round(this.center + this.center * this.wave[i] * scale);
+      var y = this.height;
+      this.beginPath();
+      this.moveTo(this.center, y);
+      this.lineTo(x, y);
+      for (i; i >= 0; i--) {
+        c1x = x;
+        c1y = y - step * 0.5;
+        c2x = Math.round(this.center + this.center * this.wave[i] * scale);
+        c2y = y - step * 0.5;
+        y = y - step;
+        x = c2x;
+        this.bezierCurveTo(c1x, c1y, c2x, c2y, x, y);
+        if (y < 0) break;
+      }
+      this.lineTo(this.center, y);
+      this.closePath();
+      this.fillStyle = color;
+      this.fill();
+    }
   });
 
 })(window);
