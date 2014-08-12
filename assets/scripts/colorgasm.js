c2cf9982b08e711f618e7bc0c203f5f3d830e155
Matthew Wagerfield <matthew@wagerfield.com>
2014-08-12 19:07:03 +0100

    :gb: Updated colorgasm.js with sketch.js stub code

--- assets/scripts/colorgasm.js
+++ assets/scripts/colorgasm.js
@@ -1,24 +1,47 @@
 (function(window) {
 
-  var COLOURS = [
-    // Base
-    0x1E1A31,
-    // Core
-    0xFFFFFF,
-    // West
-    0x6B2A64,
-    0xB14B77,
-    0xE2687C,
-    0xF98A75,
-    0xFFB46B,
-    0xFFE193,
-    // East
-    0x4C4690,
-    0x4D64B0,
-    0x518CBE,
-    0x5BAFCD,
-    0x72D4E2,
-    0xB9F7F5
-  ];
+  var COLORS = [];
+
+  function addColorPalette(id, base, core, west, east) {
+    var palette = {
+      base: base,
+      core: core,
+      east: east,
+      west: west
+    };
+  }
+
+  addColorPalette('#1E1A31', '#FFFFFF', [
+    '#6B2A64',
+    '#B14B77',
+    '#E2687C',
+    '#F98A75',
+    '#FFB46B',
+    '#FFE193'
+  ],[
+    '#4C4690',
+    '#4D64B0',
+    '#518CBE',
+    '#5BAFCD',
+    '#72D4E2',
+    '#B9F7F5'
+  ]);
+
+  Sketch.create({
+
+    setup: function() {
+      this.container = document.getElementById('container');
+      this.container.style.backgroundColor = COLOURS
+    },
+
+    update: function() {
+    },
+
+    draw: function() {
+      this.fillStyle = 'red';
+      this.fillRect(0, 0, this.width, this.height);
+    }
+
+  });
 
 })(window);
