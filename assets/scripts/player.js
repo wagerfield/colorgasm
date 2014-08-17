19e32918c8ee358b157e7c4e1b7802e83afa6eeb
Matthew Wagerfield <matthew@wagerfield.com>
2014-08-17 19:35:16 +0100

    :gb: Refactored API from Player to MediaPlayer

--- assets/scripts/player.js
+++ assets/scripts/player.js
@@ -2,176 +2,188 @@
 
   //----------------------------------------
   //
-  // PLAYER
+  // MEDIA PLAYER
   //
   //----------------------------------------
 
-  var Player = function(type) {
+  var MediaPlayer = function(type, preload) {
     this.type = /audio|video/.test(type) ? type : 'audio';
-    this.domElement = document.createElement(this.type);
-    this.paused = this.domElement.paused;
-    this.volume = this.domElement.volume;
-    this.muted = this.domElement.muted;
+    this.element = document.createElement(this.type);
+    this.paused = this.element.paused;
+    this.volume = this.element.volume;
+    this.muted = this.element.muted;
+    this.preload(preload || 'auto');
+    this.__event = new MediaPlayerEvent(this.type);
     this.__update = this.__update.bind(this);
     this.__onEvent = this.__onEvent.bind(this);
-    this.__event = new PlayerEvent(this.type);
     this.__addEventListeners();
     this.__update();
   };
 
-  window[globalID] = Player;
+  window[globalID] = MediaPlayer;
 
   //----------------------------------------
-  // PLAYER PRIVATE API
+  // MEDIA PLAYER PRIVATE API
   //----------------------------------------
 
-  Player.prototype.__addEventListeners = function() {
-    for (var i = PlayerEvent.EVENTS.length - 1; i >= 0; i--) {
-      this.domElement.addEventListener(PlayerEvent.EVENTS[i], this.__onEvent);
+  MediaPlayer.prototype.__addEventListeners = function() {
+    for (var i = MediaPlayerEvent.EVENTS.length - 1; i >= 0; i--) {
+      this.element.addEventListener(MediaPlayerEvent.EVENTS[i], this.__onEvent);
     }
   };
 
-  Player.prototype.__removeEventListeners = function() {
-    for (var i = PlayerEvent.EVENTS.length - 1; i >= 0; i--) {
-      this.domElement.removeEventListener(PlayerEvent.EVENTS[i], this.__onEvent);
+  MediaPlayer.prototype.__removeEventListeners = function() {
+    for (var i = MediaPlayerEvent.EVENTS.length - 1; i >= 0; i--) {
+      this.element.removeEventListener(MediaPlayerEvent.EVENTS[i], this.__onEvent);
     }
   };
 
-  Player.prototype.__dispatch = function(type) {
+  MediaPlayer.prototype.__dispatch = function(type) {
     this.__event.schema.type = type;
     for (var key in this.__event.schema) {
       this.__event.schema[key] = this[key];
     }
-    this.dispatchEvent(this.__event.schema);
+    // this.dispatchEvent(this.__event.schema);
   };
 
-  Player.prototype.__update = function() {
+  MediaPlayer.prototype.__update = function() {
 
     // Set buffer values
-    var buffer = this.domElement.buffered;
+    var buffer = this.element.buffered;
     this.bufferStart = buffer.length ? buffer.start(0) : 0;
     this.bufferEnd = buffer.length ? buffer.end(0) : 0;
 
     // Derive change states
-    var ready = !isNaN(this.domElement.duration);
-    var timeChanged = this.currentTime !== this.domElement.currentTime;
+    var ready = !isNaN(this.element.duration);
+    var timeChanged = this.currentTime !== this.element.currentTime;
     if (ready && timeChanged) {
 
       // Set video model progress values
-      this.currentTime = this.domElement.currentTime;
-      this.progress = this.domElement.currentTime / this.domElement.duration;
+      this.currentTime = this.element.currentTime;
+      this.progress = this.element.currentTime / this.element.duration;
 
       // Dispatch update event
-      this.__dispatch(PlayerEvent.TIME_UPDATE);
+      this.__dispatch(MediaPlayerEvent.TIME_UPDATE);
     }
 
     // Request animation frame
     this.__raf = requestAnimationFrame(this.__update);
   };
 
-  Player.prototype.__onEvent = function(event) {
+  MediaPlayer.prototype.__onEvent = function(event) {
     switch(event.type) {
-      case PlayerEvent.LOADED_META_DATA:
+      case MediaPlayerEvent.LOADED_META_DATA:
         if (this.type === 'video') {
-          this.width = this.domElement.videoWidth;
-          this.height = this.domElement.videoHeight;
+          this.width = this.element.videoWidth;
+          this.height = this.element.videoHeight;
         }
-        this.duration = this.domElement.duration;
-        this.playbackRate = this.domElement.playbackRate;
-        this.defaultPlaybackRate = this.domElement.defaultPlaybackRate;
+        this.duration = this.element.duration;
+        this.playbackRate = this.element.playbackRate;
+        this.defaultPlaybackRate = this.element.defaultPlaybackRate;
         break;
-      case PlayerEvent.PAUSE:
-      case PlayerEvent.PLAYING:
-        this.paused = this.domElement.paused;
+      case MediaPlayerEvent.PAUSE:
+      case MediaPlayerEvent.PLAYING:
+        this.paused = this.element.paused;
         break;
-      case PlayerEvent.VOLUME_CHANGE:
-        this.volume = this.domElement.volume;
-        this.muted = this.domElement.muted;
+      case MediaPlayerEvent.VOLUME_CHANGE:
+        this.volume = this.element.volume;
+        this.muted = this.element.muted;
         break;
     }
     this.__dispatch(event.type);
   };
 
   //----------------------------------------
-  // PLAYER PUBLIC API
+  // MEDIA PLAYER PUBLIC API
   //----------------------------------------
 
-  Player.prototype.destroy = function() {
-    this.__removeEventListeners();
-    this.__event = null;
-    this.domElement = null;
+  MediaPlayer.prototype.addEventListener = function(name, callback) {
   };
 
-  Player.prototype.load = function(fileURL) {
+  MediaPlayer.prototype.removeEventListener = function(name, callback) {
+  };
+
+  MediaPlayer.prototype.destroy = function() {
+    cancelAnimationFrame(this.__raf);
+    this.__removeEventListeners();
+    this.__event = null;
+    this.element = null;
+  };
+
+  MediaPlayer.prototype.load = function(fileURL) {
     if(fileURL && this.fileURL !== fileURL) {
-      this.fileURL = this.domElement.src = fileURL;
+      this.fileURL = this.element.src = fileURL;
       this.fileType = this.getFileExtension(fileURL);
-      this.domElement.load();
+      this.element.load();
     }
   };
 
-  Player.prototype.play = function() {
-    this.domElement.play();
+  MediaPlayer.prototype.play = function() {
+    this.element.play();
   };
 
-  Player.prototype.pause = function() {
-    this.domElement.pause();
+  MediaPlayer.prototype.pause = function() {
+    this.element.pause();
   };
 
-  Player.prototype.stop = function() {
+  MediaPlayer.prototype.stop = function() {
     this.pause();
     this.seek(0);
   };
 
-  Player.prototype.restart = function() {
+  MediaPlayer.prototype.restart = function() {
     this.seek(0);
     this.play();
   };
 
-  Player.prototype.seek = function(time) {
-    this.domElement.currentTime = time;
+  MediaPlayer.prototype.seek = function(time) {
+    this.element.currentTime = time;
   };
 
-  Player.prototype.scrub = function(ratio) {
-    this.seek(this.domElement.duration * ratio);
+  MediaPlayer.prototype.scrub = function(ratio) {
+    this.seek(this.element.duration * ratio);
   };
 
-  Player.prototype.loop = function(value) {
-    this.domElement.loop = value;
+  MediaPlayer.prototype.volume = function(value) {
+    this.element.volume = value;
   };
 
-  Player.prototype.volume = function(value) {
-    this.domElement.volume = value;
+  MediaPlayer.prototype.mute = function(value) {
+    this.element.muted = value;
   };
 
-  Player.prototype.mute = function(value) {
-    this.domElement.muted = value;
+  MediaPlayer.prototype.loop = function(value) {
+    this.element.loop = value;
   };
 
-  Player.prototype.togglePlayback = function() {
-    this[this.domElement.paused ? 'play' : 'pause']();
+  MediaPlayer.prototype.preload = function(value) {
+    this.element.preload = value;
   };
 
-  Player.prototype.toggleLoop = function() {
-    this.loop(!this.domElement.hasAttribute('loop'));
+  MediaPlayer.prototype.togglePlayback = function() {
+    this[this.element.paused ? 'play' : 'pause']();
   };
 
-  Player.prototype.toggleSound = function() {
-    this.mute(!this.domElement.muted);
+  MediaPlayer.prototype.toggleLoop = function() {
+    this.loop(!this.element.hasAttribute('loop'));
   };
 
-  Player.prototype.getFileExtension = function(fileURL) {
+  MediaPlayer.prototype.toggleSound = function() {
+    this.mute(!this.element.muted);
+  };
+
+  MediaPlayer.prototype.getFileExtension = function(fileURL) {
     var match = fileURL.match(/\.(\w+)$/);
     return match ? match[1] : null;
   };
 
   //----------------------------------------
   //
-  // PLAYER EVENT
+  // MEDIA PLAYER EVENT
   //
   //----------------------------------------
 
-  var PlayerEvent = function(type) {
+  var MediaPlayerEvent = function(type) {
     this.type = null;
     this.fileURL = null;
     this.fileType = null;
@@ -200,43 +212,43 @@
     }
   };
 
-  window[globalID + 'Event'] = PlayerEvent;
+  window[globalID + 'Event'] = MediaPlayerEvent;
 
   //----------------------------------------
-  // PLAYER EVENT CONSTANTS
+  // MEDIA PLAYER EVENT CONSTANTS
   //----------------------------------------
 
-  PlayerEvent.EVENTS = [
-    PlayerEvent.ABORT            = 'abort',
-    PlayerEvent.CAN_PLAY         = 'canplay',
-    PlayerEvent.CAN_PLAY_THROUGH = 'canplaythrough',
-    PlayerEvent.DURATION_CHANGE  = 'durationchange',
-    PlayerEvent.EMPTIED          = 'emptied',
-    PlayerEvent.ENDED            = 'ended',
-    PlayerEvent.ERROR            = 'error',
-    PlayerEvent.LOADED_DATA      = 'loadeddata',
-    PlayerEvent.LOADED_META_DATA = 'loadedmetadata',
-    PlayerEvent.LOAD_START       = 'loadstart',
-    PlayerEvent.PAUSE            = 'pause',
-    PlayerEvent.PLAY             = 'play',
-    PlayerEvent.PLAYING          = 'playing',
-    PlayerEvent.PROGRESS         = 'progress',
-    PlayerEvent.RATE_CHANGE      = 'ratechange',
-    PlayerEvent.RESIZE           = 'resize',
-    PlayerEvent.SEEKED           = 'seeked',
-    PlayerEvent.SEEKING          = 'seeking',
-    PlayerEvent.STALLED          = 'stalled',
-    PlayerEvent.SUSPEND          = 'suspend',
-    PlayerEvent.TIME_UPDATE      = 'timeupdate',
-    PlayerEvent.VOLUME_CHANGE    = 'volumechange',
-    PlayerEvent.WAITING          = 'waiting'
+  MediaPlayerEvent.EVENTS = [
+    MediaPlayerEvent.ABORT            = 'abort',
+    MediaPlayerEvent.CAN_PLAY         = 'canplay',
+    MediaPlayerEvent.CAN_PLAY_THROUGH = 'canplaythrough',
+    MediaPlayerEvent.DURATION_CHANGE  = 'durationchange',
+    MediaPlayerEvent.EMPTIED          = 'emptied',
+    MediaPlayerEvent.ENDED            = 'ended',
+    MediaPlayerEvent.ERROR            = 'error',
+    MediaPlayerEvent.LOADED_DATA      = 'loadeddata',
+    MediaPlayerEvent.LOADED_META_DATA = 'loadedmetadata',
+    MediaPlayerEvent.LOAD_START       = 'loadstart',
+    MediaPlayerEvent.PAUSE            = 'pause',
+    MediaPlayerEvent.PLAY             = 'play',
+    MediaPlayerEvent.PLAYING          = 'playing',
+    MediaPlayerEvent.PROGRESS         = 'progress',
+    MediaPlayerEvent.RATE_CHANGE      = 'ratechange',
+    MediaPlayerEvent.RESIZE           = 'resize',
+    MediaPlayerEvent.SEEKED           = 'seeked',
+    MediaPlayerEvent.SEEKING          = 'seeking',
+    MediaPlayerEvent.STALLED          = 'stalled',
+    MediaPlayerEvent.SUSPEND          = 'suspend',
+    MediaPlayerEvent.TIME_UPDATE      = 'timeupdate',
+    MediaPlayerEvent.VOLUME_CHANGE    = 'volumechange',
+    MediaPlayerEvent.WAITING          = 'waiting'
   ];
 
   //----------------------------------------
-  // PLAYER EVENT PUBLIC API
+  // MEDIA PLAYER EVENT PUBLIC API
   //----------------------------------------
 
-  Player.prototype.set = function(object) {
+  MediaPlayerEvent.prototype.set = function(object) {
     for (var key in this.schema) {
       if (object[key]) {
         this[key] = this.schema[key] = object[key];
@@ -244,8 +256,8 @@
     }
   };
 
-  Player.prototype.get = function() {
+  MediaPlayerEvent.prototype.get = function() {
     return this.schema;
   };
 
-})(window, 'Player');
+})(window, 'MediaPlayer');
